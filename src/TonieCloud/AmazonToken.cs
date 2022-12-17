3f2e61e04f6141a937340f601092648d4aa7abc7
gemse007 <91122342+gemse007@users.noreply.github.com>
2022-12-17 16:28:36 +0100

    Update AmazonToken.cs

--- src/TonieCloud/AmazonToken.cs
+++ src/TonieCloud/AmazonToken.cs
@@ -1,9 +1,14 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace TonieCloud
 {
     public class AmazonToken
-    {
+    { 
         public class Req
         {
             public string Url { get; set; }
@@ -13,21 +18,22 @@ namespace TonieCloud
         public class Fields
         {
             public string Key { get; set; }
-
+            
             [JsonProperty("x-amz-algorithm")]
             public string AmazonAlgorithm { get; set; }
-
+            
             [JsonProperty("x-amz-credential")]
             public string AmazonCredential { get; set; }
-
+            
             [JsonProperty("x-amz-signature")]
             public string AmazonSignature { get; set; }
-
+            
             [JsonProperty("x-amz-date")]
             public string AmazonDate { get; set; }
 
             [JsonProperty("x-amz-security-token")]
-            public string SecurityToken { get; set; }
+            public string AmazonSecurityToken { get; set; }
+            
             public string Policy { get; set; }
         }
 
